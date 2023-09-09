09e4862cb0e8196fa2fc02e834817ed986ff54ab
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-09-09 17:54:03 +0200

    mocked homePageHeader

---
+++ src/components/molecules/HomePageHeader/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+import homePageHeader from "../../../images/homePageHeader.png";
+import Logo from "../../../images/kate.jpg";
+
+const HomePageHeader = () => {
+    return (
+        <div className="relative">
+            <img src={homePageHeader} className="" alt="logo"/>
+            <div className="bg-white max-w-[518px] absolute rounded-lg -bottom-10 left-10 p-8 z-10 drop-shadow-2xl">
+                <span className="homePageTitle bg-blue-700 text-white text-sm py-0 px-1 rounded-md justify-center">Technology</span>
+                <h2 className="homePageTitle text-3xl font-semibold leading-10">The Impact of Technology on the Workplace: How Technology is Changing </h2>
+                <div className="flex justify-start items-center">
+                    <img src={Logo} className="rounded-full w-[36px] mr-2" alt="logo"/>
+                    <p className="text-xs text-zinc-700 font-medium mr-2">Katarzyna Żałoba</p>
+                    <p className="text-xs text-zinc-700">9 września 2023 roku</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePageHeader;
