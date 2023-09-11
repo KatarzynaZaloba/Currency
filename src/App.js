5a995a9a8dd7fce62932db701b544bc3b4a3b222
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-09-11 19:37:04 +0200

    added routes, and removed component from App to HomePage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.css';
 import './index.css';
 import Footer from "./components/molecules/Footer";
@@ -22,51 +23,20 @@ import HomePageHeader from "./components/molecules/HomePageHeader";
 import SectionTitle from "./components/atoms/SectionTitle";
 import OneArticleCard from "./components/organisms/OneArticleCard";
 import Button from "./components/atoms/Button";
+import HomePage from "./pages/HomePage";
+import ArticlePage from "./pages/ArticlePage";
 
 function App() {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [numberOfPosts, setNumberOfPosts] = useState(6);
-    const sortedArticles = [...articlesLink].sort((a, b) => b.id - a.id);
-    const filteredArticles = sortedArticles.filter(
-        article =>
-            (typeof article.title === 'string' && article.title.toLowerCase().includes(searchTerm.toLowerCase())) ||
-            (typeof article.body === 'string' && article.body.toLowerCase().includes(searchTerm.toLowerCase())) ||
-            (typeof article.date === 'string' && article.date.toLowerCase().includes(searchTerm.toLowerCase()))
-    );
-    const articlesToDisplay = filteredArticles.slice(0, numberOfPosts);
 
     return (
-        <div>
+        <Router>
             <Container>
-                <NavBar/>
-                <HomePageHeader/>
-                <Section
-                    title="Ostatnie posty"
-                    subtitle=""
-                    body={
-                        <>
-                        <div className="grid md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
-                            {articlesToDisplay.map((article) => (
-                                <OneArticleCard
-                                    key={article.id}
-                                    date={article.date}
-                                    title={article.title}
-                                    body={article.body}
-                                />
-                            ))}
-                        </div>
-                            <div className="flex justify-center items-center my-6">
-                                <Button text="Zobacz więcej" onClick={() => {
-                                    setNumberOfPosts(numberOfPosts + 6);
-                                    console.log("Kliknięto przycisk, nowa liczba postów to:", numberOfPosts + 6);
-                                }} />
-                            </div>
-                        </>
-                    }
-                />
+                <Routes>
+                    <Route path="/article/:id" element={<ArticlePage />} />
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
             </Container>
-            <Footer/>
-        </div>
+        </Router>
     );
 }
 
---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+import '../../App.css';
+import '../../index.css';
+import Footer from "../../components/molecules/Footer";
+import AboutMe from "../../components/templates/AboutMe";
+import Section from "../../components/organisms/Section";
+import Article from "../../components/molecules/Article";
+import Container from "../../components/atoms/Container";
+import Main from "../../components/atoms/Main";
+import {
+    articlesLink
+} from "../../components/molecules/Article/ArticleData";
+import NavBarTitle from "../../components/atoms/NavBarTitle";
+import SearchBar from "../../components/molecules/SearchBar";
+import SwitchButton from "../../components/molecules/SwitchButton";
+import NavBar from "../../components/organisms/NavBar";
+import FooterSectionTitle from "../../components/atoms/FooterSectionTitle";
+import FooterSectionUndertitle from "../../components/atoms/FooterSectionUndertitle";
+import FooterSectionParagraph from "../../components/atoms/FooterSectionParagraph";
+import FooterContactTitle from "../../components/atoms/FooterContactTitle";
+import HomePageHeader from "../../components/molecules/HomePageHeader";
+import SectionTitle from "../../components/atoms/SectionTitle";
+import OneArticleCard from "../../components/organisms/OneArticleCard";
+import Button from "../../components/atoms/Button";
+
+function HomePage() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [numberOfPosts, setNumberOfPosts] = useState(6);
+
+    const sortedArticles = [...articlesLink].sort((a, b) => b.id - a.id);
+    const newestArticle = sortedArticles[0];
+    const filteredArticles = sortedArticles.filter(
+        article =>
+            (typeof article.title === 'string' && article.title.toLowerCase().includes(searchTerm.toLowerCase())) ||
+            (typeof article.body === 'string' && article.body.toLowerCase().includes(searchTerm.toLowerCase())) ||
+            (typeof article.date === 'string' && article.date.toLowerCase().includes(searchTerm.toLowerCase()))
+    );
+    const articlesToDisplay = filteredArticles.slice(0, numberOfPosts);
+
+    return (
+        <>
+        <Container>
+            <NavBar/>
+            <HomePageHeader
+                newestArticle={newestArticle}/>
+            <Section
+                title="Ostatnie posty"
+                subtitle=""
+                body={
+                    <>
+                        <div className="grid md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1">
+                            {articlesToDisplay.map((article) => (
+                                <OneArticleCard
+                                    key={article.id}
+                                    date={article.date}
+                                    title={article.title}
+                                    body={article.body}
+                                />
+                            ))}
+                        </div>
+                        <div className="flex justify-center items-center my-6">
+                            <Button text="Zobacz więcej" onClick={() => {
+                                setNumberOfPosts(numberOfPosts + 6);
+                                console.log("Kliknięto przycisk, nowa liczba postów to:", numberOfPosts + 6);
+                            }} />
+                        </div>
+                    </>
+                }
+            />
+        </Container>
+    <Footer/>
+    </>
+);
+}
+
+export default HomePage;
