4a67876e56d3a59a8721dda6f7a256973818e4b5
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-09-12 09:02:57 +0200

    commented links which are not done yet

--- src/components/organisms/NavBar/index.js
+++ src/components/organisms/NavBar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 import NavBarTitle from "../../atoms/NavBarTitle";
 import SearchBar from "../../molecules/SearchBar";
 import SwitchButton from "../../molecules/SwitchButton";
@@ -10,23 +11,23 @@ const NavBar = () => {
                 {/*<div>*/}
                 {/*    /!*Logo*!/*/}
                 {/*</div>*/}
-                <div className="md:hidden m-auto flex justify-end items-center">
-                    <SwitchButton/>
-                </div>
+                {/*<div className="md:hidden m-auto flex justify-end items-center">*/}
+                {/*    <SwitchButton/>*/}
+                {/*</div>*/}
                 <div className="bg-white text-center md:text-start md:flex md:justify-start justify-center gap-x-10">
-                    <NavBarTitle title="Home"/>
-                    <NavBarTitle title="Blog"/>
-                    <NavBarTitle title="Posty"/>
-                    <NavBarTitle title="O mnie"/>
-                    <NavBarTitle title="Kontakt"/>
+                    <Link to="/blog"><NavBarTitle title="Home"/></Link>
+                    {/*<NavBarTitle title="Blog"/>*/}
+                    {/*<NavBarTitle title="Posty"/>*/}
+                    {/*<NavBarTitle title="O mnie"/>*/}
+                    {/*<NavBarTitle title="Kontakt"/>*/}
                 </div>
-                <div className="md:flex justify-end items-center gap-x-3 md:gap-x-10 hidden">
-                    <SearchBar className="ml-5"/>
-                    <SwitchButton/>
-                </div>
-                <div className="md:hidden m-auto flex justify-center items-center">
-                    <SearchBar />
-                </div>
+                {/*<div className="md:flex justify-end items-center gap-x-3 md:gap-x-10 hidden">*/}
+                {/*    <SearchBar className="ml-5"/>*/}
+                {/*    <SwitchButton/>*/}
+                {/*</div>*/}
+                {/*<div className="md:hidden m-auto flex justify-center items-center">*/}
+                {/*    <SearchBar />*/}
+                {/*</div>*/}
             </div>
         </>
     )
