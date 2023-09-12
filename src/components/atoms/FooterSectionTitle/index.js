41ef51a540368272148c74d5e586cb2c2cc51d1d
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-09-12 08:52:48 +0200

    rearranged Footer

--- src/components/atoms/FooterSectionTitle/index.js
+++ src/components/atoms/FooterSectionTitle/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 const FooterSectionTitle = ({title}) => {
     return (
-   <p className="footerSectionTitle text-base font-semibold leading-7 text-zinc-900">{title}</p>
+   <p className="footerSectionTitle text-base font-semibold leading-7 text-zinc-900 pb-2">{title}</p>
     )
 }
 
--- src/components/molecules/Footer/index.js
+++ src/components/molecules/Footer/index.js
@@ -8,47 +8,47 @@ import FooterSectionUndertitle from "../../atoms/FooterSectionUndertitle";
 const Footer = () => {
     return (
         <footer className="bg-zinc-100 m-auto px-5 md:pb-10 pb-5 md:px-40">
-            <div className="grid grid-cols-4 gap-4">
-                <div className="col-span-2">
+            <div className="py-4 grid grid-cols-4 gap-4">
+                <div className="py-4 lg:col-span-2 col-span-4">
                     <div className="break-words">
                         <FooterSectionTitle title="O projekcie"/>
                         <FooterSectionParagraph
                             text="Jest to blog, który powstawał na przełomie kilku miesięcy. Piszę w nim o bieganiu oraz powtarzam i utrwalam materiał dotyczący web-developmentu. Chętnie wspomogę Cię także w Twoim projekcie!"/>
                     </div>
-                    <div>
+                    <div className="pt-4">
                         <Link tonClick={() => window.location = 'mailto:kzalobafrontend@gmail.com'}><FooterSectionTitle title="Email: kzalobafrontend@gmail.com"/></Link>
                         {/*<FooterSectionTitle title="Phone: 880 123 456 789"/>*/}
                     </div>
                 </div>
-                <div>
-                    <FooterSectionTitle title="Quick Link"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
+                {/*<div>*/}
+                {/*    <FooterSectionTitle title="Quick Link"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
 
-                </div>
-                <div>
-                    <FooterSectionTitle title="Category"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                    <FooterSectionUndertitle title="Lifestyle"/>
-                </div>
+                {/*</div>*/}
+                {/*<div>*/}
+                {/*    <FooterSectionTitle title="Category"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*    <FooterSectionUndertitle title="Lifestyle"/>*/}
+                {/*</div>*/}
             </div>
-            <div className="flex justify-between items-center border-t-2">
-                <div>
-                    UI/ UX Design:
+            <div className="flex justify-between items-center border-t-2 py-4">
+                <div className="flex justify-center items-center">
+                    <p className="font-sm pr-2">UX UI Design:</p>
                     <a href="https://www.jstemplate.net/" className="text-zinc-900 font-semibold">JS Template</a>
                 </div>
-                <div className="flex justify-end items-center">
-                    <FooterSectionUndertitle title="Polityka prywatności"/>
-                    <FooterSectionUndertitle title="Polityka ciasteczek"/>
-                </div>
+                {/*<div className="flex justify-end items-center">*/}
+                {/*    <FooterSectionUndertitle title="Polityka prywatności"/>*/}
+                {/*    <FooterSectionUndertitle title="Polityka ciasteczek"/>*/}
+                {/*</div>*/}
             </div>
         </footer>
     )
