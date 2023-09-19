0a6c0190eb137d89ca750d2bee7590af739c8cf1
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-09-19 11:25:42 +0200

    changed background of the Figure

--- src/components/atoms/Figure/index.js
+++ src/components/atoms/Figure/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Figure = ({ children }) => {
     return (
-        <div className="flex justify-center bg-emerald-50 py-4 m-4">
+        <div className="flex justify-center bg-zinc-50 py-4 m-4">
         <figure className="m-auto text-center w-1/1 md:w-1/2 p-4">
             {children}
         </figure>
