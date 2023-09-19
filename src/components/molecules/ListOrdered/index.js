f10db86640c967cde45e3f66ce7ad83e445f56b6
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-09-19 11:26:29 +0200

    refactored ListOrdered

--- src/components/molecules/ListOrdered/index.js
+++ src/components/molecules/ListOrdered/index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 const ListOrdered = ({ children }) => {
     return (
-        <ol className="list-decimal">
+        <ol className="list-decimal marker:text-zinc-400 md:leading-8 md-leading-6 text-left ml-2.5 mt-0 mb-5">
             {children}
         </ol>
     )
