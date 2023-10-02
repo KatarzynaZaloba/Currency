e720808addc3f0277d5ecf8894f35d02cbddb014
Katarzyna Zaloba <123471023+KatarzynaZaloba@users.noreply.github.com>
2023-10-02 08:46:06 +0200

    fixed hover effect in button

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -4,7 +4,7 @@ import './style.css';
 const Button = ({ text, onClick }) => {
     return (
         <>
-            <button onClick={onClick} className="border-2 text-work-sans rounded-md text-zinc-700 text-base font-medium p-2 text-center w-48 leading-6 ease-in duration-300 hover:text-indigo-500 active:text-indigo-400 visited:text-indigo-900 hover:animate-bounce">
+            <button onClick={onClick} className="transition duration-300 ease-in-out hover:scale-110 border-2 text-work-sans rounded-md text-zinc-700 text-base font-medium p-2 text-center w-48 leading-6 active:text-indigo-400 visited:text-indigo-900">
                 {text}
             </button>
         </>
